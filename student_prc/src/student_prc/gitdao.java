92af259c3de740d714892149c7d2aa6cd64bd514
liaojianyun <1806022301@qq.com>
2020-06-10 10:55:46 +0800

    用户对DLL的操作

---
+++ student_prc/src/student_prc/gitdao.java
@@ -0,0 +1,20 @@
+package student_prc;
+
+import java.util.List;
+
+public class gitdao {
+
+	public void addUser(int id,String username,String password) {
+		
+	}
+	public void deleteUser(int id) {
+		
+	}
+	public void updateUser(int id,String newUsrname,String newPassword) {
+		
+	}
+	public List<Object> getUsers(int pageIndex,int pageSize){
+		
+		return null;
+	}
+}
